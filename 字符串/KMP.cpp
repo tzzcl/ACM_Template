d596a3510220d46031c616dc77cf9718b2b2124b
tzzcl <zclnjucs@gmail.com>
2014-05-07 14:59:39 +0800

    KMP Signed-off-by: tzzcl <zclnjucs@gmail.com>

---
+++ 字符串/KMP.cpp
@@ -0,0 +1,23 @@
+int getFail(char p[],int f[]){
+	int m = strlen(p);
+	f[0] = f[1] = 0;
+	for(int i = 1;i<m;i++){
+		int j = f[i];
+		while(j&&p[i]!=p[j]) j = f[j];
+		f[i+1] = p[i]==p[j]?j+1:0;
+	}
+	return m;
+}
+
+//sÊÇÆ¥Åä´®£¬pÊÇÄ£°æ´®
+bool find(char s[],char p[],int f[]){
+	int n = strlen(s);
+	int m = getFail(p,f);
+	int j = 0;
+	for(int i = 0;i<n;i++){
+		while(j&&p[j]!=s[i]) j = f[j];
+		if(p[j]==s[i]) j++;
+		if(j==m) return true;
+	}
+	return false;
+}
