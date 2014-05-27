24bc57bf7697d277cca4aace309cf9f6e449319f
tzzcl <zclnjucs@gmail.com>
2014-05-27 13:57:55 +0800

    BigDecimal Sqrt(Newton Method) Signed-off-by: tzzcl <zclnjucs@gmail.com>

---
+++ Java/BigDecimal Sqrt.java
@@ -0,0 +1,18 @@
+private static final BigDecimal SQRT_DIG = new BigDecimal(150);
+	private static final BigDecimal SQRT_PRE = new BigDecimal(10).pow(SQRT_DIG.intValue());
+	public static BigDecimal sqrtNewtonRaphson  (BigDecimal c, BigDecimal xn, BigDecimal precision){
+	    BigDecimal fx = xn.pow(2).add(c.negate());
+	    BigDecimal fpx = xn.multiply(new BigDecimal(2));
+	    BigDecimal xn1 = fx.divide(fpx,2*SQRT_DIG.intValue(),RoundingMode.HALF_DOWN);
+	    xn1 = xn.add(xn1.negate());
+	    BigDecimal currentSquare = xn1.pow(2);
+	    BigDecimal currentPrecision = currentSquare.subtract(c);
+	    currentPrecision = currentPrecision.abs();
+	    if (currentPrecision.compareTo(precision) <= -1){
+	        return xn1;
+	    }
+	    return sqrtNewtonRaphson(c, xn1, precision);
+	}
+	public static BigDecimal sqrt(BigDecimal c){
+	    return sqrtNewtonRaphson(c,new BigDecimal(1),new BigDecimal(1).divide(SQRT_PRE));
+	}
